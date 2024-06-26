728a7fbdc8f2cbeefb9b4bc83acc9272961b4b3a
sudhir_kumar <sudhirme101@gmail.com>
2024-06-26 13:55:25 +0530

    hamburgur logic added

--- client/src/layout/Headers/Header.jsx
+++ client/src/layout/Headers/Header.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Dropdown from 'react-bootstrap/Dropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 import './header.scss'
 export const Header = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
     <header>
@@ -16,7 +21,7 @@ export const Header = () => {
           </div>
         </div>
         <div className="right">
-        <div className="hamburgur">
+        <div className="hamburgur" onClick={handleShow}>
         <i class="fa fa-bars"></i>
         </div>
           <div className="nav_btn">
@@ -56,6 +61,24 @@ export const Header = () => {
         </div>
       </nav>
     </div>
+
+    <Offcanvas  show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+        <img src="/profile-image.png" alt="" width='40px'  className='profile_img'/>
+        </Offcanvas.Header>
+        <Offcanvas.Body style={{background:'black',color:'white'}}>
+        <a style={{textDecoration:'none', color:'white'}} href=""> <i class="fa-solid fa-shop">&nbsp;&nbsp;&nbsp;</i>Products</a> <br /> <br/>
+
+        <a style={{textDecoration:'none', color:'white'}} href=""> <i class="fa-solid fa-circle-arrow-right">&nbsp;&nbsp;&nbsp;</i>Login</a> <br />
+        <div id='ex4' className="cartsicon">
+            <a className='text-light' href="">
+              <span className='p1 fa-stack fa-2x has-badge' data-count={0}>
+              <i class=" p1 fa-solid fa-cart-shopping"></i>
+              </span>
+            </a>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
 
     </header>
 
